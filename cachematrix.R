e943cdcec2b3af59786f77677a14fcf721fc87d2
abalone0204 <abalone0204@gmail.com>
2015-03-06 23:48:45 +0800

    Edit args in solve function

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,19 @@
 
 ## Write a short comment describing this function
 
+# Assuem that all of our input matrix are nonsingular
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_x <- NULL
+    set <- function(y) {
+        x <<- y
+        inv_x <<- NULL
+    }
+    get <- function() x
+    set_inverse <- function(x)  inv_x <<- solve(x,1)
+    get_inverse <- function() inv_x
+    list(set = set, get = get, 
+        set_inverse = set_inverse,
+        get_inverse = get_inverse)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv_x <- x$get_inverse()
+        if(!is.null(inv_x)) {
+            message("Getting cached data")
+            return(inv_x)
+        }
+        data <- x$get()
+        inv_x <- solve(data, 1)
+        x$set_inverse(inv_x)
+        inv_x
 }
